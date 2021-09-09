53a2c73ff6722f61c8d6e9f4e62d3c20098f0b11
Denis <Dysprosium92@ya.ru>
2021-09-09 22:02:22 +0300

    Added function load_font

--- include/assets.h
+++ include/assets.h
@@ -1,8 +1,23 @@
-//
-// Created by dosart on 9/9/21.
-//
-
 #ifndef TETRIS_INCLUDE_ASSETS_H_
 #define TETRIS_INCLUDE_ASSETS_H_
+
+#include <string>
+#include <optional>
+
+namespace game{
+/**
+ * @brief Load font from resources
+ *
+ * @param fontName Name of font for load
+ *
+ * @return SFML format font
+ */
+inline std::optional<sf::Font> load_font(std::string fontName) {
+  sf::Font font;
+  if (!font.loadFromFile(fontName))
+    return {};
+  return font;
+}
+}
 
 #endif //TETRIS_INCLUDE_ASSETS_H_
