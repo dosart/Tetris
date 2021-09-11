1459f3b9fdea3aa4be410ea42511756e9a45fb23
Denis <Dysprosium92@ya.ru>
2021-09-11 21:57:32 +0300

    Added function load_texture. Changed fileName -> file_name

--- include/assets.h
+++ include/assets.h
@@ -4,7 +4,7 @@
 #include <string>
 #include <optional>
 
-namespace game{
+namespace game {
 /**
  * @brief Load font from resources
  *
@@ -12,11 +12,18 @@ namespace game{
  *
  * @return SFML format font
  */
-inline std::optional<sf::Font> load_font(std::string fontName) {
+inline std::optional<sf::Font> load_font(std::string file_path) {
   sf::Font font;
-  if (!font.loadFromFile(fontName))
+  if (!font.loadFromFile(file_path))
     return {};
   return font;
+}
+
+inline std::optional<sf::Texture> load_texture(std::string file_path) {
+  sf::Texture texture;
+  if (!texture.loadFromFile(file_path))
+    return {};
+  return texture;
 }
 }
 
