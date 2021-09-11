504b458469caae8df16d05dfb44f08fa6a918f04
Denis <Dysprosium92@ya.ru>
2021-09-11 22:06:16 +0300

    Changed main function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include "render.h"
+#include "controller.h"
+#include "assets.h"
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+
+  if (auto font = game::load_font("../resources/calibri.ttf"); font.has_value()) {
+
+    if (auto texture = game::load_texture("../resources/tiles.png"); texture.has_value()) {
+      game::render_t render(std::move(font.value()), std::move(texture.value()));
+      game::controller controller(&render);
+
+      controller.run();
+    }
+  }
+
   return 0;
 }
+
