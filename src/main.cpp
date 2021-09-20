629d38c4b904ae45692052856f3831bfbc840fd7
Denis <Dysprosium92@ya.ru>
2021-09-20 13:15:18 +0300

    Resources loader created in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,19 @@
 #include "render.h"
 #include "controller.h"
 #include "assets.h"
+#include "AssetsLoader.hpp"
 
 int main() {
 
-  auto font = game::load_font("../resources/calibri.ttf");
-  if (!font.has_value())
-    return -1;
+  ::Loader::AssetsLoader<sf::Texture> texture_loader;
+  texture_loader.setSupportedFormats("png", "[]");
+  texture_loader.loadAssets("../resources", [](auto &item, auto path) { item.loadFromFile(path.string()); });
 
-  auto texture = game::load_texture("../resources/tiles.png");
-  if (!texture.has_value())
-    return -1;
+  ::Loader::AssetsLoader<sf::Font> font_loader;
+  font_loader.setSupportedFormats("ttf", "[]");
+  font_loader.loadAssets("../resources", [](auto &item, auto path) { item.loadFromFile(path.string()); });
 
-  game::render_t render(std::move(font.value()), std::move(texture.value()));
+  game::render_t render(font_loader.getPtr("calibri"), texture_loader.getPtr("tiles"));
   game::controller controller(&render);
   controller.run();
 
