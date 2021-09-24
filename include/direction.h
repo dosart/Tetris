352ac02ea575fffebd536957fefffb0528ac92c4
Denis <Dysprosium92@ya.ru>
2021-09-24 20:26:08 +0300

    Added enum for moving along A and Y axes

---
+++ include/direction.h
@@ -0,0 +1,8 @@
+#ifndef TETRIS_INCLUDE_DIRECTION_H_
+#define TETRIS_INCLUDE_DIRECTION_H_
+
+namespace game {
+enum class Direction { LEFT, RIGHT, UD, DOWN };
+}
+
+#endif //TETRIS_INCLUDE_DIRECTION_H_
