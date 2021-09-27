cc3b4e62e9e3f5d608dc8bc685e335d5c732d505
Denis <Dysprosium92@ya.ru>
2021-09-27 20:40:53 +0300

    Change UD for UP

--- include/direction.h
+++ include/direction.h
@@ -2,7 +2,7 @@
 #define TETRIS_INCLUDE_DIRECTION_H_
 
 namespace game {
-enum class Direction { LEFT, RIGHT, UD, DOWN };
+enum class Direction { LEFT, RIGHT, UP, DOWN };
 }
 
 #endif //TETRIS_INCLUDE_DIRECTION_H_
