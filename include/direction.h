2ccc8f4545a3de1330597c9a056c2d5c91e03a97
Denis <Dysprosium92@ya.ru>
2021-09-28 19:34:23 +0300

    deleted:    include/direction.h

--- include/direction.h
+++
@@ -1,8 +0,0 @@
-#ifndef TETRIS_INCLUDE_DIRECTION_H_
-#define TETRIS_INCLUDE_DIRECTION_H_
-
-namespace game {
-enum class Direction { LEFT, RIGHT, UP, DOWN };
-}
-
-#endif //TETRIS_INCLUDE_DIRECTION_H_
